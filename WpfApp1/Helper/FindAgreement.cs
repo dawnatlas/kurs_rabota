6529d1c498d9b87bae8c26ce3252e18a01897689
Анастасия Аникина <102908197+dawnatlas@users.noreply.github.com>
2022-04-13 22:23:38 +0300

    Add files via upload

---
+++ WpfApp1/Helper/FindAgreement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Model;
+
+namespace WpfApp1.Helper
+{
+    class FindAgreement
+    {
+        public int id;
+        public FindAgreement(int id)
+        {
+            this.id = id;
+        }
+        public bool AgreementPredicate(Agreement agr)
+        {
+            return agr.Id == id;
+        }
+    }
+}
---
+++ WpfApp1/Helper/FindBank.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Model;
+
+namespace WpfApp1.Helper
+{
+    class FindBank
+    {
+        public int id;
+        public FindBank(int id)
+        {
+            this.id = id;
+        }
+        public bool BankPredicate(Bank bank)
+        {
+            return bank.Id == id;
+        }
+    }
+}
