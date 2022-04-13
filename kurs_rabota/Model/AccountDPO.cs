a5612366781555e809a93fe54fc3de27fe2df9eb
Анастасия Аникина <102908197+dawnatlas@users.noreply.github.com>
2022-04-13 22:15:29 +0300

    Add files via upload

---
+++ kurs_rabota/Model/AccountDPO.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.ViewModel;
+
+namespace WpfApp1.Model
+{
+    class AccountDPO
+    {
+        public int Id { get; set; }
+        public string a_Type { get; set; }
+        public string a_Bank { get; set; }
+        public string a_Agreement { get; set; }
+        public string Account_ { get; set; }
+
+        public AccountDPO() { }
+        public AccountDPO(int id, string a_Type, string a_Bank,
+            string a_Agreement, string Account_)
+        {
+            this.Id = id;
+            this.a_Type = a_Type;
+            this.a_Bank = a_Bank;
+            this.a_Agreement = a_Agreement;
+            this.Account_ = Account_;
+        }
+        public AccountDPO CopyFromAccount(Account account)
+        {
+            AccountDPO acDPO = new AccountDPO();
+            TypeAccountViewModel vmTypeAccount = new TypeAccountViewModel();
+            BankViewModel vmBank = new BankViewModel();
+            AgreementViewModel vmAgreement = new AgreementViewModel();
+            string bank = string.Empty;
+            string TA = string.Empty;
+            string agr = string.Empty;
+            foreach (var t in vmTypeAccount.ListTypeAccount)
+            {
+                if (t.Id == account.Id)
+                {
+                    TA = t.TypeAccount_;
+                    break;
+                }
+            }
+            foreach (var b in vmBank.ListBank)
+            {
+                if (b.Id == account.Id)
+                {
+                    bank = b.NameShort;
+                    break;
+                }
+            }
+            foreach (var a in vmAgreement.ListAgreement)
+            {
+                if (a.Id == account.Id)
+                {
+                    agr = a.Number;
+                    break;
+                }
+            }
+            if (TA != string.Empty && bank != string.Empty && agr != string.Empty)
+            {
+                acDPO.Id = account.Id;
+                acDPO.a_Type = TA;
+                acDPO.a_Bank = bank;
+                acDPO.a_Agreement = agr;
+                acDPO.Account_ = account.Account_;
+            }
+            return acDPO;
+        }
+        public AccountDPO ShallowCopy()
+        {
+            return (AccountDPO)this.MemberwiseClone();
+        }
+    }
+}
---
+++ kurs_rabota/ViewModel/AgreementViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Model;
+using System.Collections.ObjectModel;
+
+namespace WpfApp1.ViewModel
+{
+    class AgreementViewModel
+    {
+        public ObservableCollection<Agreement> ListAgreement { get; set; } = new ObservableCollection<Agreement>();
+
+        public AgreementViewModel()
+        {
+            this.ListAgreement.Add(
+                new Agreement
+                {
+                    Id = 1,
+                    Number = "А-005015",
+                    DataOpen = new DateTime(2018, 03, 20),
+                    DataClouse = new DateTime(2022, 03, 20),
+                    Note = "Срок годности карты-4 года"
+
+                });
+
+            this.ListAgreement.Add(
+    new Agreement
+    {
+        Id = 2,
+        Number = "Ф-005024",
+        DataOpen = new DateTime(2016, 10, 07),
+        DataClouse = new DateTime(2026, 10, 07),
+        Note = "Срок годности карты-10 лет"
+
+    });
+this.ListAgreement.Add(
+new Agreement
+            {
+                Id = 3,
+                Number = "К-003118",
+                DataOpen = new DateTime(2020, 07, 09),
+                DataClouse = new DateTime(2022, 07, 09),
+                Note = "Срок годности карты-2 года"
+
+            });
+        }
+        public int MaxId()
+        {
+            int max = 0;
+            foreach (var r in this.ListAgreement)
+            {
+                if (max < r.Id)
+                {
+                    max = r.Id;
+                };
+            }
+            return max;
+        }
+    }
+}
